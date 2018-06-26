42a1cf50a1a40c3312f9ebc3841f91c4ff34f728
Augusto <augusto.gonzaga2010@gmail.com>
2018-06-26 12:34:56 -0300

    update gradle and refactor intent open sdk > 24

--- android/src/main/java/com/fileopener/FileOpener.java
+++ android/src/main/java/com/fileopener/FileOpener.java
@@ -1,66 +1,69 @@
 package com.fileopener;
 
-import java.io.File;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.content.FileProvider;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import java.util.Map;
+import org.json.JSONException;
+
+import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 
 public class FileOpener extends ReactContextBaseJavaModule {
 
-  public FileOpener(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+    public FileOpener(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  @Override
-  public String getName() {
-    return "FileOpener";
-  }
+    @Override
+    public String getName() {
+        return "FileOpener";
+    }
 
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    return constants;
-  }
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        return constants;
+    }
 
-  @ReactMethod
-  public void open(String fileArg, String contentType, Promise promise) throws JSONException {
-  		File file = new File(fileArg);
+    @ReactMethod
+    public void open(String fileArg, String contentType, Promise promise) throws JSONException {
+        File file = new File(fileArg);
 
-  		if (file.exists()) {
-  			try {
-          Uri path = FileProvider.getUriForFile(getReactApplicationContext(), getReactApplicationContext().getPackageName() + ".fileprovider", file);
-  				Intent intent = new Intent(Intent.ACTION_VIEW);
-  				intent.setDataAndType(path, contentType);
-          intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-          intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-  				getReactApplicationContext().startActivity(intent);
+        if (file.exists()) {
+            try {
+                Uri path = FileProvider.getUriForFile(getReactApplicationContext(), getReactApplicationContext().getPackageName() + ".fileprovider", file);
+                if (Build.VERSION.SDK_INT >= 24) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW).setDataAndType(path, contentType);
+                    intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                    PackageManager pm = getCurrentActivity().getPackageManager();
+                    if (intent.resolveActivity(pm) != null) {
+                        this.getReactApplicationContext().startActivity(intent);
+                    }
+
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(Uri.parse("file://" + fileArg), contentType).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    this.getReactApplicationContext().startActivity(intent);
+
+                }
 
                 promise.resolve("Open success!!");
-  			} catch (android.content.ActivityNotFoundException e) {
+            } catch (android.content.ActivityNotFoundException e) {
                 promise.reject("Open error!!");
-  			}
-  		} else {
+            }
+        } else {
             promise.reject("File not found");
-  		}
-  	}
+        }
+    }
 
 }
